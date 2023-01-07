dd244c157cfbffb5087dc943a3b10246ee508813
ongkay <bisnismilyarandollar@gmail.com>
2023-01-07 20:37:42 +0700

    nprogress + scroll restoration

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,20 @@
 import '../styles/globals.css'
+import NextNProgress from 'nextjs-progressbar'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <NextNProgress
+        color="red"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={false}
+        options={{ showSpinner: false }}
+      />
+      <Component {...pageProps} />;
+    </>
+  )
 }
 
 export default MyApp
